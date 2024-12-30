f94ac7729b35aa76432470a56249217286cc318f
Gavin Smith <39228744+gavinhsmith@users.noreply.github.com>
2024-12-29 23:49:38 -0600

    Added some tools to make tests work

--- src/module/date.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Gets the current time.
- * @param adjust Adjusts the current time by `ms` milliseconds.
- * @returns A instance of `Date` that holds the time.
- */
-export default function getCurrentTime(adjust: number = 0): Date {
-  return adjust != 0 ? new Date() : new Date(new Date().getTime() + adjust);
-}
--- src/module/index.ts
+++ src/module/index.ts
@@ -1,6 +1,62 @@
-import getCurrentTime from "./date.js";
+// Types / Interfaces
 
-console.info("Hello, World!");
-console.info(`It is currently ${getCurrentTime().toDateString()}.`);
+/** A function that will be run on shutdown. */
+export type ShutdownProcesser = () => void;
 
-process.exit(0);
+/** A shutdown process and its requires. */
+export interface ShutdownProcess {
+  processer: ShutdownProcesser;
+  requires: string[];
+}
+
+/** Config for shutdown. */
+export interface ShutdownConfig {
+  /** The exit code that SIGTERM, SIGINT, etc should send when recieved. */
+  signalExitCode?: number;
+}
+
+// Constants
+
+/** The default config for shutdown. */
+const DEFAULT_CONFIG: ShutdownConfig = {
+  signalExitCode: 1,
+};
+
+/** A collection of all known processes. */
+const SHUTDOWN_PROCESSES: Map<string, ShutdownProcess> = new Map();
+
+// Helper Functions
+
+// Exports
+
+/**
+ * Initiate shutdown.
+ * @param config Config for shutdown.
+ */
+export function init(config: ShutdownConfig = {}): void {
+  const { signalExitCode }: ShutdownConfig = { ...DEFAULT_CONFIG, ...config };
+  const processSigEvent = () => process.exit(signalExitCode);
+
+  process.on("SIGTERM", processSigEvent);
+  process.on("SIGINT", processSigEvent);
+  process.on("SIGABRT", processSigEvent);
+
+  process.on("exit", () => {});
+}
+
+/**
+ * Adds a new process to be run at shutdown.
+ * @param name The name of this process
+ * @param shutdownProcess The function that will be run.
+ * @param requires A list of processes that need to be completed before this one can be run.
+ */
+export function runAtShutdown(
+  name: string,
+  shutdownProcess: ShutdownProcesser,
+  requires: string[] = []
+) {
+  SHUTDOWN_PROCESSES.set(name, {
+    processer: shutdownProcess,
+    requires,
+  });
+}
